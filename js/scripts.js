10d1ee8bda9fb0961f32a304b75595756dcb8b73
Jeff Kim <kim2jy@hotmail.com>
2020-06-19 11:25:44 -0700

    Create constructor for pizza object and begin prototype for price calculator (pizza size)

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,32 @@
 // Business Logic
+function PizzaOrder(name, size, toppings, receive) {
+  this.name = name;
+  this.size = size;
+  this.toppings = toppings;
+  this.receive = receive;
+}
 
+PizzaOrder.prototype.priceCalculator() {
+  let price = 0;
+  // Size price calculation
+  if (this.size === "Small") {
+    price = 7;
+  }
+  else if (this.size === "Medium") {
+    price = 9;
+  }
+  else if (this.size === "Large") {
+    price = 11;
+  }
+  else if (this.size === "X-Large") {
+    price = 13;
+  }
+  return price;
+
+  // Toppings price calculation
+
+  // Receive price calculation
+}
 // User Interface Logic
 $(document).ready(function() {
 
